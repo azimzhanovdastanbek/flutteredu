72cb2f7a622e74048cebd5f825fe55bcaa5a4268
azimzhanovdastanbek <azimzhanovdastanbek@gmail.com>
2021-03-18 17:09:50 +0600

    Fixed bug

--- dart/lesson4_stdin_stdout2.dart
+++ dart/lesson4_stdin_stdout2.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 void main() {
   stdout.write("Введите Ваш номер:");
   var tel = double.parse(stdin.readLineSync().toString());
-  if (tel == 0.0) {
+  if (tel % 2 == 0.0) {
     stdout.write('У Вас четный номер.');
   } else {
     stdout.write('У Вас нечетный номер.');
